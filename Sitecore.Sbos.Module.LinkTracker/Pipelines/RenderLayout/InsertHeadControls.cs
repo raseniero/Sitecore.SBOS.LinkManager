1e3ded714c525bb66630e6741276fa18cb66259f
Robert van den Boorn <rvb@sitecore.net>
2017-07-24 11:36:14 +0200

    Updated config and restored pipeline.

---
+++ Sitecore.Sbos.Module.LinkTracker/Pipelines/RenderLayout/InsertHeadControls.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Mvc.Pipelines.Response.RenderRendering;
+
+namespace Sitecore.Sbos.Module.LinkTracker.Pipelines.RenderLayout
+{
+    public class InsertHeadControls : RenderRenderingProcessor
+    {
+        public override void Process(RenderRenderingArgs args)
+        {
+            if (Sitecore.Context.Site.Name == "shell")
+            {
+                return;
+            }
+
+            if (!args.Writer.ToString().Contains(Data.Constants.LinkTrackerConstants.LinkTrackerMgdJSPath))
+            {
+                if (args.Writer.ToString().Contains("<title>"))
+                {
+                    args.Writer.WriteLine(Data.Constants.LinkTrackerConstants.JQueryScript);
+                    args.Writer.WriteLine(Data.Constants.LinkTrackerConstants.LinkTrackerMgrScript);
+                }
+            }
+        }
+    }
+}
